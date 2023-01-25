f8af96ec922ca8bf7259064238d221c1010a4619
hhhEEElll <rtuezptim@mail.ru>
2023-01-25 07:29:32 +0300

    Добавьте файлы проекта.

---
+++ Паттерн MVVM/Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Паттерн_MVVM
+{
+    public static class Model
+    {
+        // блок с данными
+        public static string firstField; // Переменная для хранения значения первого поля ввода
+        public static string secondlyField; // Переменная для хранения значения второго поля ввода
+        public static string result = ""; // Переменная для хранения результата
+        public static List<string> dataListDisplay = new List<string>() { "Сложение", "Вычитание", "Умножение", "Деление" }; // Список для выбора из comboBox
+        public static List<string> dataListValue = new List<string>() { "+", "-", "*", "/" }; // Список для вывода в textBox
+    }
+}
---
+++ Паттерн MVVM/ViewModel.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Windows;
+
+namespace Паттерн_MVVM
+{
+    class ViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string FirstField // Свойство для заполнение значения первого поля
+        {
+            set
+            {
+                Model.firstField = value;
+            }
+        }
+        public string SecondlyField // Свойство для заполнение значения второго поля
+        {
+            set
+            {
+                Model.secondlyField = value;
+            }
+        }
+
+        public string ResultChange // Свойство для отображения результата вычисления в TextBlock
+        {
+            get
+            {
+                return Model.result.ToString();
+            }
+        }
+
+        public List<String> ComboChange  // Свойство для заполнения Combobox
+        {
+            get
+            {
+                return Model.dataListDisplay;
+            }
+        }
+        int cbIndex = -1;
+        public int IndexSelected // свойство для нахождения индекса выбранного в Combobox элемента
+        {
+            set
+            {
+                // индек - это необходимое значение, которое нужно получить
+                cbIndex = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("CBIndex"));  // событие, которое реагирует на изменение свойства
+            }
+        }
+
+        public string CBIndex // Свойство для отображения выбранной арифметической операции
+        {
+            get
+            {
+                if (cbIndex == -1)
+                {
+                    return "";
+                }
+                else
+                {
+                    return Model.dataListValue[cbIndex];
+                }
+            }
+        }
+
+        public RoutedCommand Command { get; set; } = new RoutedCommand();
+
+        public void Command_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                double numberFirst = 0; // Переменная, которая хранит числовое значение первого поля
+                double numberSecondly = 0; // Переменная, которая хранит числовое значение второго поля
+                if (Model.firstField != "")
+                {
+                    numberFirst = Convert.ToDouble(Model.firstField);
+                }
+                if (Model.secondlyField != "")
+                {
+                    numberSecondly = Convert.ToDouble(Model.secondlyField);
+                }
+                switch (cbIndex)
+                {
+                    case -1:
+                        MessageBox.Show("Арифметическая операция не выбрана");
+                        return;
+                    case 0:
+                        Model.result = Convert.ToString(numberFirst + numberSecondly);
+                        break;
+                    case 1:
+                        Model.result = Convert.ToString(numberFirst - numberSecondly);
+                        break;
+                    case 2:
+                        Model.result = Convert.ToString(numberFirst * numberSecondly);
+                        break;
+                    case 3:
+                        if (numberSecondly == 0)
+                        {
+                            Model.result = "Деление на 0 (ноль) не допустимо";
+                        }
+                        else
+                        {
+                            Model.result = Convert.ToString(numberFirst / numberSecondly);
+                        }
+                        break;
+                    default:
+                        MessageBox.Show("При расчёте возникла ошибка");
+                        return;
+                }
+                PropertyChanged(this, new PropertyChangedEventArgs("ResultChange"));
+            }
+            catch
+            {
+                MessageBox.Show("При вычисление арифметической операции возникла ошибка");
+            }
+        }
+        public CommandBinding bind;
+        public ViewModel()
+        {
+            bind = new CommandBinding(Command);
+            bind.Executed += Command_Executed;
+        }
+    }
+}
